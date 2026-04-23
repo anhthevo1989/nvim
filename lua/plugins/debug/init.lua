f5985454d5a52a9b37297ba49da672b5632ceeb1
anhthevo1989 <anhthevo.1989@gmail.com>
2026-04-23 10:49:12 -0400

    feat(debug): complete debugging system with dap, ui, and osv
    - configured nvim-dap core debugging engine
    - added nvim-dap-ui for visual debugging interface
    - enabled nvim-dap-virtual-text for inline variable display
    - integrated one-small-step-for-vimkind (osv) for Lua debugging
    - implemented full debug flow (launch, attach, breakpoints, stepping)
    
    debugging system is now fully functional

--- lua/plugins/debug/init.lua
+++ lua/plugins/debug/init.lua
@@ -3,31 +3,58 @@
 -- ==========================================================
 -- PURPOSE
 -- -------
--- Debugging system using nvim-dap
---
--- WHAT THIS DOES
--- --------------
--- - Installs core debugging engine
--- - Adds basic debugging keymaps
+-- Debugging system using nvim-dap + OSV (Lua debugger)
 -- ==========================================================
 
 return {
 	{
 		"mfussenegger/nvim-dap",
 
+		dependencies = {
+			"jbyuki/one-small-step-for-vimkind",
+		},
+
 		config = function()
 			local dap = require("dap")
+			local osv = require("osv")
 
 			-- ======================================================
 			-- KEYMAPS
 			-- ======================================================
 
-			vim.keymap.set("n", "<F5>", dap.continue)
+			vim.keymap.set("n", "<F5>", function()
+				local dap = require("dap")
+				local osv = require("osv")
+
+				osv.launch({ port = 8086 })
+				dap.continue()
+			end)
+
 			vim.keymap.set("n", "<F10>", dap.step_over)
 			vim.keymap.set("n", "<F11>", dap.step_into)
 			vim.keymap.set("n", "<F12>", dap.step_out)
 
 			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+
+			-- ======================================================
+			-- ADAPTER
+			-- ======================================================
+
+			dap.adapters.nlua = function(callback, config)
+				callback({
+					type = "server",
+					host = "127.0.0.1",
+					port = 8086,
+				})
+			end
+
+			dap.configurations.lua = {
+				{
+					type = "nlua",
+					request = "attach",
+					name = "Attach to running Neovim instance",
+				},
+			}
 		end,
 	},
 }
